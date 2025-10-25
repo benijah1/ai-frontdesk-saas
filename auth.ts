677b296f24364e604ceea38eaeadd8bebfbda8c5
benijah1 <ijah.benjamin@gmail.com>
2025-10-25 11:45:12 -0700

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 // auth.ts
 import { getServerSession } from "next-auth";
 import type { NextAuthOptions } from "next-auth";
-import { authOptions } from "./app/api/auth/[...nextauth]/route";
+import { authOptions } from "./app/api/auth/auth-options";
 
 export async function auth() {
   return getServerSession(authOptions as NextAuthOptions);
