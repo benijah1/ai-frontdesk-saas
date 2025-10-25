fd4b20898c3e8a6ba159d2d897919c8f1affc9bb
benijah1 <ijah.benjamin@gmail.com>
2025-10-24 17:27:51 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,10 @@
 export { default } from "next-auth/middleware";
 export const config = {
-  matcher: ["/dashboard/:path*"]
+  matcher: [
+    "/dashboard/:path*",
+    "/leads/:path*",
+    "/calls/:path*",
+    "/settings/:path*",
+    "/crm/:path*",
+  ],
 };
